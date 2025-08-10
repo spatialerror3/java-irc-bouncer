d768382c84d9b3fcce5ba2a11efb33463d18b317
spatialerror3 <>
2025-08-10 18:34:28 +0200

    new

---
+++ src/main/java/net/spatialerror3/jib/JIBJython.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package net.spatialerror3.jib;
+
+import org.python.util.PythonInterpreter;
+
+/**
+ *
+ * @author spatialerror3
+ */
+public class JIBJython {
+    public JIBJython() {
+        PythonInterpreter interp =
+	    new PythonInterpreter();
+    }
+}
