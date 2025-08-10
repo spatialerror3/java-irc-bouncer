a06cc54d7c98ca2af5018f305ba0a70ee7bf483c
spatialerror3 <ss@linux.fritz.box>
2025-08-10 18:23:03 +0200

    new

---
+++ src/main/java/net/spatialerror3/jib/JIBConfig.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package net.spatialerror3.jib;
+
+import java.util.Hashtable;
+
+/**
+ *
+ * @author spatialerror3
+ */
+public class JIBConfig {
+    private Hashtable<String,String> kvStore = null;
+    
+    public JIBConfig() {
+        kvStore = new Hashtable<String,String>();
+    }
+    
+    public void parseArgs(String[] args) {
+        if(args.length > 0) {
+            setValue("Server", args[0]);
+        }
+    }
+    
+    public void setValue(String Key, String Value) {
+        kvStore.put(Key, Value);
+    }
+    
+    public String getValue(String Key) {
+        return kvStore.get(Key);
+    }
+}
