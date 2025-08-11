b73cebb0e6996637064b6a6a92e987f196dd3893
spatialerror3 <>
2025-08-11 17:34:47 +0200

    docker

--- src/main/java/net/spatialerror3/jib/JIBJython.java
+++ src/main/java/net/spatialerror3/jib/JIBJython.java
@@ -4,6 +4,7 @@
  */
 package net.spatialerror3.jib;
 
+import java.io.File;
 import org.python.util.PythonInterpreter;
 
 /**
@@ -11,21 +12,25 @@ import org.python.util.PythonInterpreter;
  * @author spatialerror3
  */
 public class JIBJython {
+
     public static PythonInterpreter interp = null;
-    
+
     public JIBJython() {
         interp = new PythonInterpreter();
         interp.set("a", this);
     }
-    
+
     public void loadConfig() {
-        interp.execfile("./config.jy");
+        File cf = new File("./config.jy");
+        if (cf.exists() && !cf.isDirectory()) {
+            interp.execfile("./config.jy");
+        }
     }
-    
+
     public JIBServer rJIBServ() {
         return JavaIrcBouncer.jibServ;
     }
-    
+
     public JIBConfig rJIBConfig() {
         return JavaIrcBouncer.jibConfig;
     }
