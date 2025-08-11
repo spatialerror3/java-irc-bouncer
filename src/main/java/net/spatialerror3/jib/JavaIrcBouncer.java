2872abf1cd1b85a65882a1f48a837788f6030230
spatialerror3 <>
2025-08-11 15:31:31 +0200

    new

--- src/main/java/net/spatialerror3/jib/JavaIrcBouncer.java
+++ src/main/java/net/spatialerror3/jib/JavaIrcBouncer.java
@@ -9,15 +9,24 @@ package net.spatialerror3.jib;
  * @author spatialerror3
  */
 public class JavaIrcBouncer {
+    public static JIBConfig jibConfig = null;
+    public static JIBDBUtil jibDbUtil = null;
     public static JIBServer jibServ = null;
     public static JIBIRC jibIRC = null;
+    public static JIBJython jibJython = null;
 
     public static void main(String[] args) {
+        jibConfig = new JIBConfig();
+        jibConfig.parseArgs(args);
+        jibDbUtil = new JIBDBUtil();
+        jibDbUtil.initSchema();
         JIBServer jib1 = null;
         jib1 = new JIBServer();
         JavaIrcBouncer.jibServ=jib1;
         Thread t1 = new Thread(jib1);
         t1.start();
+        jibJython = new JIBJython();
+        jibJython.loadConfig();
         while(true) {
             try {
                 Thread.sleep(60000);
@@ -25,6 +34,5 @@ public class JavaIrcBouncer {
                 System.getLogger(JavaIrcBouncer.class.getName()).log(System.Logger.Level.ERROR, (String) null, ex);
             }
         }
-        //System.out.println("Hello World!");
     }
 }
