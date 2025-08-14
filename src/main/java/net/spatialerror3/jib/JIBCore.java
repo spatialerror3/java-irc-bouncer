7b12906a9799d6f410ded801fae14e241067ed19
spatialerror3 <spatialerror3@protonmail.com>
2025-08-15 00:02:18 +0200

    template

---
+++ src/main/java/net/spatialerror3/jib/JIBCore.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package net.spatialerror3.jib;
+
+/**
+ *
+ * @author spatialerror3
+ */
+public class JIBCore {
+    
+}
