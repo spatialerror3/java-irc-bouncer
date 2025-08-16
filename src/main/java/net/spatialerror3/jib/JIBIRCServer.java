77b10ce6e5bb305d4e9fec7f4618c573e4681456
spatialerror3 <spatialerror3@protonmail.com>
2025-08-16 14:36:21 +0200

    irc

--- src/main/java/net/spatialerror3/jib/JIBIRCServer.java
+++ src/main/java/net/spatialerror3/jib/JIBIRCServer.java
@@ -4,55 +4,73 @@
  */
 package net.spatialerror3.jib;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 /**
  *
  * @author spatialerror3
  */
 public class JIBIRCServer {
+
     private JIBIRCNetType.NetType netType = JIBIRCNetType.NetType.GENERIC;
     private String server = null;
     private int port = 0;
     private boolean ssl = true;
     //
     private String clientBind = null;
-    
+    //
+    private InetAddress resolved = null;
+
     public JIBIRCServer() {
-        
+
     }
-    
+
     public JIBIRCNetType.NetType getNetType() {
         return this.netType;
     }
-    
+
     public String getServer() {
         return this.server;
     }
-    
+
     public int getPort() {
         return this.port;
     }
-    
+
     public boolean getSsl() {
         return this.ssl;
     }
-    
+
     public String getClientBind() {
         return this.clientBind;
     }
-    
+
     public void setNetType(JIBIRCNetType.NetType netType) {
-        this.netType=netType;
+        this.netType = netType;
     }
-    
+
     public void setServer(String server) {
-        this.server=server;
+        this.server = server;
     }
-    
+
     public void setPort(int port) {
-        this.port=port;
+        this.port = port;
+    }
+
+    public void setSsl(boolean ssl) {
+        this.ssl = ssl;
+    }
+
+    public void resolve() {
+        try {
+            this.resolved = InetAddress.getByName(this.server);
+        } catch (UnknownHostException ex) {
+            System.getLogger(JIBIRC.class.getName()).log(System.Logger.Level.ERROR, (String) null, ex);
+        }
     }
     
-    public void setSsl(boolean ssl) {
-        this.ssl=ssl;
+    public InetAddress getResolved() {
+        return this.resolved;
     }
 }
