8c444c1182fa30027ac845ca251c2f994642cda9
spatialerror3 <spatialerror3@protonmail.com>
2025-08-16 14:03:29 +0200

    irc

---
+++ src/main/java/net/spatialerror3/jib/JIBIRCNetType.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package net.spatialerror3.jib;
+
+/**
+ *
+ * @author spatialerror3
+ */
+public class JIBIRCNetType {
+    public enum NetType {
+        GENERIC,
+        LIBERA,
+        OFTC,
+        DALNET,
+        UNDERNET,
+        QUAKENET,
+        HYBRID,
+        INSPIRCD,
+        UNREALIRCD
+    }
+}
