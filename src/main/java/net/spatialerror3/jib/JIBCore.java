507b8256ff51d2a21d60b8c21fdfa4ef00e4a7fc
spatialerror3 <spatialerror3@protonmail.com>
2025-08-16 13:14:49 +0200

    multiple users

--- src/main/java/net/spatialerror3/jib/JIBCore.java
+++ src/main/java/net/spatialerror3/jib/JIBCore.java
@@ -5,6 +5,7 @@
 package net.spatialerror3.jib;
 
 import java.util.Hashtable;
+import java.util.UUID;
 import java.util.Vector;
 
 /**
@@ -14,22 +15,29 @@ import java.util.Vector;
 public class JIBCore {
     Vector<JIBUser> users = null;
     Hashtable<String,JIBUser> userMap = null;
+    Hashtable<UUID,JIBUser> userMap2 = null;
     
     public JIBCore() {
         users = new Vector<JIBUser>();
         userMap = new Hashtable<String,JIBUser>();
+        userMap2 = new Hashtable<UUID,JIBUser>();
     }
     
     public JIBUser createUser(String userName, boolean admin) {
         JIBUser u = new JIBUser();
         u.setUserName(userName);
         userMap.put(userName, u);
+        userMap2.put(u.getUUID(), u);
         users.add(u);
         return u;
     }
     
     public JIBUser getUser(String userName) {
         return userMap.get(userName);
+    }
+    
+    public JIBUser getUser(UUID uuid) {
+        return userMap2.get(uuid);
     }
     
     public JIBUser authUser(String userName, String authToken) {
