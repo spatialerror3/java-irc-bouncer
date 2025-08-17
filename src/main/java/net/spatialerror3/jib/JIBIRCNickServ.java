04870275434d08e1aa8716763e25b3eecc363449
spatialerror3 <spatialerror3@protonmail.com>
2025-08-17 15:20:10 +0200

    fix

--- src/main/java/net/spatialerror3/jib/JIBIRCNickServ.java
+++ src/main/java/net/spatialerror3/jib/JIBIRCNickServ.java
@@ -12,9 +12,13 @@ public class JIBIRCNickServ {
 
     private String nickServAccount = "";
     private String nickServPass = "";
+    //
+    private JIBUser u = null;
+    private JIBIRCServer serv = null;
 
-    public JIBIRCNickServ() {
-
+    public JIBIRCNickServ(JIBUser u, JIBIRCServer serv) {
+        this.u=u;
+        this.serv=serv;
     }
 
     public void init() {
@@ -24,7 +28,7 @@ public class JIBIRCNickServ {
 
     public void identify() {
         if (nickServAccount != null && nickServPass != null) {
-            JavaIrcBouncer.jibIRC.writeLine("PRIVMSG NickServ :IDENTIFY " + nickServAccount + " " + nickServPass + "\r\n");
+            u.getJibIRC().writeLine("PRIVMSG NickServ :IDENTIFY " + nickServAccount + " " + nickServPass + "\r\n");
         }
     }
 }
