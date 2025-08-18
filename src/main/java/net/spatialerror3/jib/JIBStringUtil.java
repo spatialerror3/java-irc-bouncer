de7cd835bfdee8d7b7929ac5bbd97f7df76a8519
spatialerror3 <spatialerror3@protonmail.com>
2025-08-18 20:34:39 +0200

    random hex strings

--- src/main/java/net/spatialerror3/jib/JIBStringUtil.java
+++ src/main/java/net/spatialerror3/jib/JIBStringUtil.java
@@ -4,6 +4,11 @@
  */
 package net.spatialerror3.jib;
 
+import java.util.Random;
+import java.util.UUID;
+import java.util.random.RandomGenerator;
+import org.apache.commons.codec.digest.DigestUtils;
+
 /**
  *
  * @author spatialerror3
@@ -26,5 +31,16 @@ public class JIBStringUtil {
             return s.substring(1);
         }
         return s;
+    }
+    
+    public static String randHexString() {
+        String hexStr = null;
+        String randStr1 = UUID.randomUUID().toString();
+        String randStr2 = Long.toString(System.currentTimeMillis());
+        String randStr3 = null;
+        RandomGenerator g = RandomGenerator.of("L64X128MixRandom");
+        randStr3=Long.toString(g.nextLong());
+        hexStr=DigestUtils.sha256Hex(randStr3+randStr2+randStr1);
+        return hexStr;
     }
 }
