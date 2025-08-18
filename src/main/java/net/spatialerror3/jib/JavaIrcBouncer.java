365b4923cbeebe488ffe9dc23964942c23772a8a
spatialerror3 <spatialerror3@protonmail.com>
2025-08-18 20:35:05 +0200

    defaults

--- src/main/java/net/spatialerror3/jib/JavaIrcBouncer.java
+++ src/main/java/net/spatialerror3/jib/JavaIrcBouncer.java
@@ -1,7 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package net.spatialerror3.jib;
 
 /**
@@ -9,6 +8,7 @@ package net.spatialerror3.jib;
  * @author spatialerror3
  */
 public class JavaIrcBouncer {
+    
     public static JIBCore jibCore = null;
     public static JIBConfig jibConfig = null;
     public static JIBSysEnv jibSysEnv = null;
@@ -16,7 +16,7 @@ public class JavaIrcBouncer {
     public static JIBServer jibServ = null;
     public static JIBIRC jibIRC = null;
     public static JIBJython jibJython = null;
-
+    
     public static void main(String[] args) {
         System.setProperty("python.import.site", "false");
         jibCore = new JIBCore();
@@ -37,13 +37,21 @@ public class JavaIrcBouncer {
         jibDbUtil.initSchema();
         jibJython = new JIBJython();
         jibJython.loadConfig();
-        jibCore.createUser(jibConfig.getValue("AUTHUSER"), true).setAuthToken(jibConfig.getValue("AUTHPASS"));
+        if (jibConfig.getValue("AUTHPASS") == null) {
+            jibConfig.setValue("AUTHPASS", JIBStringUtil.randHexString());
+            System.err.println("Setting AUTHPASS for admin user to: " + jibConfig.getValue("AUTHPASS"));
+        }
+        if (jibConfig.getValue("AUTHUSER") != null) {
+            jibCore.createUser(jibConfig.getValue("AUTHUSER"), true).setAuthToken(jibConfig.getValue("AUTHPASS"));
+        } else {
+            jibCore.createUser("admin", true).setAuthToken(jibConfig.getValue("AUTHPASS"));
+        }
         JIBServer jib1 = null;
         jib1 = new JIBServer();
-        JavaIrcBouncer.jibServ=jib1;
+        JavaIrcBouncer.jibServ = jib1;
         Thread t1 = new Thread(jib1);
         t1.start();
-        while(true) {
+        while (true) {
             try {
                 Thread.sleep(60000);
             } catch (InterruptedException ex) {
