b587b71f6b8ea77421ac18af22073271584d91fa
spatialerror3 <spatialerror3@protonmail.com>
2025-08-20 18:49:52 +0200

    fix

--- src/main/java/net/spatialerror3/jib/JIBIRCNickServ.java
+++ src/main/java/net/spatialerror3/jib/JIBIRCNickServ.java
@@ -17,18 +17,28 @@ public class JIBIRCNickServ {
     private JIBIRCServer serv = null;
 
     public JIBIRCNickServ(JIBUser u, JIBIRCServer serv) {
-        this.u=u;
-        this.serv=serv;
+        this.u = u;
+        this.serv = serv;
     }
 
     public void init() {
-        nickServAccount = JavaIrcBouncer.jibConfig.getValue("NICKSERVUSER");
-        nickServPass = JavaIrcBouncer.jibConfig.getValue("NICKSERVPASS");
+        if (u.getUserId() == 0 && u.admin() == true) {
+            nickServAccount = JavaIrcBouncer.jibConfig.getValue("NICKSERVUSER");
+            nickServPass = JavaIrcBouncer.jibConfig.getValue("NICKSERVPASS");
+        } else {
+            nickServAccount = serv.getNickServUser();
+            nickServPass = serv.getNickServPass();
+        }
     }
 
     public void identify() {
         if (nickServAccount != null && nickServPass != null) {
-            u.getJibIRC().writeLine("PRIVMSG NickServ :IDENTIFY " + nickServAccount + " " + nickServPass + "\r\n");
+            if (serv.getNetType().equals(JIBIRCNetType.NetType.LIBERA)) {
+                u.getJibIRC().writeLine("PRIVMSG NickServ :IDENTIFY " + nickServAccount + " " + nickServPass + "\r\n");
+            }
+            if (serv.getNetType().equals(JIBIRCNetType.NetType.OFTC)) {
+                u.getJibIRC().writeLine("PRIVMSG NickServ :IDENTIFY " + nickServPass + "\r\n");
+            }
         }
     }
 }
