4afbb9c0b62f0404c89657716021c550aaf1541c
spatialerror3 <spatialerror3@protonmail.com>
2025-08-23 15:52:35 +0200

    fix

--- src/main/java/net/spatialerror3/jib/JIBShutdown.java
+++ src/main/java/net/spatialerror3/jib/JIBShutdown.java
@@ -32,6 +32,7 @@ public class JIBShutdown extends Thread {
     }
     
     public void run() {
-        
+        log.debug("Shutting down...");
+        JavaIrcBouncer.jibQuartz.shutdown();
     }
 }
