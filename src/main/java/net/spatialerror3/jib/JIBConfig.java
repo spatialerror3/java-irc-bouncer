2ca6194ea71dd104d032766a2d501d0abd0cb671
spatialerror3 <spatialerror3@protonmail.com>
2025-08-30 12:46:53 +0200

    fix

--- src/main/java/net/spatialerror3/jib/JIBConfig.java
+++ src/main/java/net/spatialerror3/jib/JIBConfig.java
@@ -26,33 +26,35 @@ import java.util.Hashtable;
  * @author spatialerror3
  */
 public class JIBConfig implements Serializable {
+
     private static final long serialVersionUID = 1L;
-    private HashMap<String,String> kvStore = null;
-    
+    private HashMap<String, String> kvStore = null;
+
     public JIBConfig() {
-        kvStore = new HashMap<String,String>();
+        kvStore = new HashMap<String, String>();
     }
-    
+
     public void parseArgs(String[] args) {
-        if(args.length > 0) {
+        if (args.length > 0) {
             setValue("Server", args[0]);
         }
     }
-    
+
     public void setValue(String Key, String Value) {
-        if(Value!=null)
-          kvStore.put(Key, Value);
+        if (Value != null) {
+            kvStore.put(Key, Value);
+        }
     }
-    
+
     public String getValue(String Key) {
         return kvStore.get(Key);
     }
-    
+
     public void createUser(String userName, String pass) {
         JIBUser u = JavaIrcBouncer.jibCore.createUser(userName, false);
         u.setAuthToken(pass);
     }
-    
+
     public void createUser(String userName, String pass, String n, String id, String r, String server, int port) {
         JIBUser u = JavaIrcBouncer.jibCore.createUser(userName, false);
         u.setAuthToken(pass);
@@ -65,7 +67,20 @@ public class JIBConfig implements Serializable {
         tmpServ.setSsl(true);
         u.addIrcServer(tmpServ);
     }
-    
+
+    public void createUser(String userName, String pass, String n, String id, String r, String server, String port) {
+        JIBUser u = JavaIrcBouncer.jibCore.createUser(userName, false);
+        u.setAuthToken(pass);
+        u.setNick(n);
+        u.setUser(id);
+        u.setRealname(r);
+        JIBIRCServer tmpServ = new JIBIRCServer();
+        tmpServ.setServer(server);
+        tmpServ.setPort(port);
+        //tmpServ.setSsl(true);
+        u.addIrcServer(tmpServ);
+    }
+
     public void createUser(String userName, String pass, String n, String id, String r, String server, int port, String nsacct, String nspass, String chans) {
         JIBUser u = JavaIrcBouncer.jibCore.createUser(userName, false);
         u.setAuthToken(pass);
@@ -76,6 +91,22 @@ public class JIBConfig implements Serializable {
         tmpServ.setServer(server);
         tmpServ.setPort(port);
         tmpServ.setSsl(true);
+        tmpServ.setNickServUser(nsacct);
+        tmpServ.setNickServPass(nspass);
+        tmpServ.addChannels(chans);
+        u.addIrcServer(tmpServ);
+    }
+
+    public void createUser(String userName, String pass, String n, String id, String r, String server, String port, String nsacct, String nspass, String chans) {
+        JIBUser u = JavaIrcBouncer.jibCore.createUser(userName, false);
+        u.setAuthToken(pass);
+        u.setNick(n);
+        u.setUser(id);
+        u.setRealname(r);
+        JIBIRCServer tmpServ = new JIBIRCServer();
+        tmpServ.setServer(server);
+        tmpServ.setPort(port);
+        //tmpServ.setSsl(true);
         tmpServ.setNickServUser(nsacct);
         tmpServ.setNickServPass(nspass);
         tmpServ.addChannels(chans);
