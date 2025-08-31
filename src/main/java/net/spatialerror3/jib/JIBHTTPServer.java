9576ee63e6b07adfdeb4e8b50c876f5f97a993b2
spatialerror3 <spatialerror3@protonmail.com>
2025-08-31 21:02:14 +0200

    fix

--- src/main/java/net/spatialerror3/jib/JIBHTTPServer.java
+++ src/main/java/net/spatialerror3/jib/JIBHTTPServer.java
@@ -24,6 +24,7 @@ import org.eclipse.jetty.ee10.servlet.DefaultServlet;
 import org.eclipse.jetty.ee10.servlet.ResourceServlet;
 import org.eclipse.jetty.ee10.servlet.ServletContextHandler;
 import org.eclipse.jetty.ee10.servlet.ServletHolder;
+import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.HttpConnectionFactory;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
@@ -39,92 +40,30 @@ import org.eclipse.jetty.util.resource.ResourceFactory;
  *
  * @author spatialerror3
  */
-public class JIBHTTPServer {
+public class JIBHTTPServer extends JIBWebBase {
 
     private static final Logger log = LogManager.getLogger(JIBHTTPServer.class);
-    private int Port = -1;
+    //
 
     public JIBHTTPServer(int Port) {
         if (Port == -1) {
-            this.Port = 7680;
+            setPort(7680, "http");
         }
         //
         Server server = new Server();
-
-        DefaultSessionIdManager idMgr = new DefaultSessionIdManager(server);
-        idMgr.setWorkerName("server1");
-        server.addBean(idMgr, true);
-        //try {
-        //    idMgr.start();
-        //} catch (Exception e3) {
-        //    log.error((String) null, e3);
-        //}
-
-        try {
-            HouseKeeper houseKeeper = new HouseKeeper();
-            houseKeeper.setSessionIdManager(idMgr);
-            //set the frequency of scavenge cycles
-            houseKeeper.setIntervalSec(600L);
-            idMgr.setSessionHouseKeeper(houseKeeper);
-            //server.addBean(houseKeeper, true);
-        } catch (IllegalStateException ise2) {
-            log.debug("HouseKeeper IllegalStateException ise2", ise2);
-        } catch (Exception e2) {
-            log.error((String) null, e2);
-        }
+        setServer(server,"server1");
+        
+        addSessionIdManager();
 
         int acceptors = 4;
         int selectors = 4;
         ServerConnector connector = new ServerConnector(server, acceptors, selectors, new HttpConnectionFactory());
-        connector.setPort(this.Port);
+        connector.setPort(getPort());
         connector.setHost("0.0.0.0");
         connector.setAcceptQueueSize(128);
         server.addConnector(connector);
         
-        ServletContextHandler handler = new ServletContextHandler("/", true, true);
-        URL staticResources = getClass().getClassLoader().getResource("static");
-        handler.setBaseResourceAsString(staticResources.toExternalForm());
-
-        //handler.addServlet(JIBHTTPServletLogin.class.getName(), "/");
-        handler.addServlet(JIBHTTPServletLogin.class.getName(), "/login");
-        handler.addServlet(JIBHTTPServletLogin.class.getName(), "/login/");
-        handler.addServlet(JIBHTTPServletServers.class.getName(), "/servers");
-        handler.addServlet(JIBHTTPServletServers.class.getName(), "/servers/");
-        handler.addServlet(JIBHTTPServletUsers.class.getName(), "/users");
-        handler.addServlet(JIBHTTPServletUsers.class.getName(), "/users/");
-        handler.addServlet(JIBHTTPServletStatus.class.getName(), "/status");
-        handler.addServlet(JIBHTTPServletStatus.class.getName(), "/status/");
-        handler.addServlet(JIBHTTPServletLogout.class.getName(), "/logout");
-        handler.addServlet(JIBHTTPServletLogout.class.getName(), "/logout/");
-        
-        final ServletHolder defaultHolder = new ServletHolder("default", ResourceServlet.class);
-        defaultHolder.setInitParameter("baseResource", staticResources.toExternalForm());
-        handler.addServlet(defaultHolder, "/static/*");
-        handler.addServlet(defaultHolder, "/static/JIB.png");
-        
-        ResourceHandler handler3 = new ResourceHandler();
-        handler3.setBaseResource(ResourceFactory.of(handler3).newResource("./static/"));
-        handler3.setAcceptRanges(true);
-        //server.setDefaultHandler(handler3);
-                
-        ContextHandlerCollection contextCollection = new ContextHandlerCollection();
-        contextCollection.addHandler(new ContextHandler(handler3, "/static"));
-        contextCollection.addHandler(new ContextHandler(handler, "/login"));
-        contextCollection.addHandler(new ContextHandler(handler, "/login/"));
-        contextCollection.addHandler(new ContextHandler(handler, "/servers"));
-        contextCollection.addHandler(new ContextHandler(handler, "/servers/"));
-        contextCollection.addHandler(new ContextHandler(handler, "/users"));
-        contextCollection.addHandler(new ContextHandler(handler, "/users/"));
-        contextCollection.addHandler(new ContextHandler(handler, "/status"));
-        contextCollection.addHandler(new ContextHandler(handler, "/status/"));
-        contextCollection.addHandler(new ContextHandler(handler, "/logout"));
-        contextCollection.addHandler(new ContextHandler(handler, "/logout/"));
-        server.setHandler(contextCollection);
-        server.setDefaultHandler(contextCollection);
-        contextCollection.deployHandler(handler, Callback.NOOP);
-        
-        //server.setDefaultHandler(new JIBHTTPHandler());
-        //server.setDefaultHandler(handler);
+        addHandlers();
         try {
             server.start();
         } catch (UnsupportedOperationException uoe1) {
@@ -132,5 +71,9 @@ public class JIBHTTPServer {
         } catch (Exception ex) {
             log.error("Error Starting HTTP Server", ex);
         }
+    }
+    
+    public String toString() {
+        return super.toString();
     }
 }
