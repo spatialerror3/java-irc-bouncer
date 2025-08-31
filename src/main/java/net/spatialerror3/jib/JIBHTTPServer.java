9a748b625c5d647dc1ab999602556494bf80e598
spatialerror3 <spatialerror3@protonmail.com>
2025-08-31 15:32:11 +0200

    fix

--- src/main/java/net/spatialerror3/jib/JIBHTTPServer.java
+++ src/main/java/net/spatialerror3/jib/JIBHTTPServer.java
@@ -75,6 +75,7 @@ public class JIBHTTPServer {
         ServletContextHandler handler = new ServletContextHandler("/", true, true);
         handler.addServlet(JIBHTTPServletLogin.class.getName(), "/");
         handler.addServlet(JIBHTTPServletServers.class.getName(), "/servers");
+        handler.addServlet(JIBHTTPServletUsers.class.getName(), "/users");
         handler.addServlet(JIBHTTPServletStatus.class.getName(), "/status");
         handler.addServlet(JIBHTTPServletLogout.class.getName(), "/logout");
         var defaultServlet = handler.addServlet(DefaultServlet.class, "/static/*");
