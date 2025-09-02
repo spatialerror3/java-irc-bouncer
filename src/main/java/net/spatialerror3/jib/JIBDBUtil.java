8b5a0b60a06e1d7065613ce1838030324fecaebb
spatialerror3 <spatialerror3@protonmail.com>
2025-09-02 20:12:42 +0200

    fix

--- src/main/java/net/spatialerror3/jib/JIBDBUtil.java
+++ src/main/java/net/spatialerror3/jib/JIBDBUtil.java
@@ -23,6 +23,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.UUID;
 import java.util.Vector;
 import org.apache.logging.log4j.LogManager;
@@ -34,12 +36,12 @@ import org.h2.tools.Server;
  * @author spatialerror3
  */
 public class JIBDBUtil {
-
+    
     private static final Logger log = LogManager.getLogger(JIBDBUtil.class);
     private String dbFile = null;
     private Connection conn = null;
     private Server server = null;
-
+    
     public JIBDBUtil(String dbFile) {
         log.debug("JIBDBUtil() this=" + this);
         this.dbFile = dbFile;
@@ -54,11 +56,11 @@ public class JIBDBUtil {
         }
         log.debug("JIBDBUtil() this=" + this + " conn=" + this.conn);
     }
-
+    
     public void shutdown() {
-
+        
     }
-
+    
     public Connection getDatabase() {
         if (conn == null) {
             try {
@@ -69,9 +71,9 @@ public class JIBDBUtil {
         }
         return conn;
     }
-
+    
     public void initSchema() {
-        String sql = "CREATE TABLE IF NOT EXISTS servers (id int auto_increment primary key, server varchar(256), port integer, ssl boolean, ipv6 boolean, nick varchar(256), username varchar(256), realname varchar(256), nsacct varchar(256), nspass varchar(256), channels varchar(512), u varchar(256), opt object(10000000));";
+        String sql = "CREATE TABLE IF NOT EXISTS servers (id int auto_increment primary key, server varchar(256), port integer, ssl boolean, ipv6 boolean, clientbind varchar(256), serverpass varchar(256), nick varchar(256), username varchar(256), realname varchar(256), nsacct varchar(256), nspass varchar(256), channels varchar(512), u varchar(256), s varchar(256), opt object(10000000));";
         try {
             PreparedStatement ps1 = getDatabase().prepareStatement(sql);
             ps1.execute();
@@ -112,10 +114,10 @@ public class JIBDBUtil {
             log.error((String) null, e);
         }
     }
-
+    
     public int cntResultSet(ResultSet rs) {
         int r = -1;
-
+        
         try {
             rs.last();
             r = rs.getRow();
@@ -124,10 +126,10 @@ public class JIBDBUtil {
             log.error((String) null, e);
             r = 0;
         }
-
+        
         return r;
     }
-
+    
     public long getUsersMaxUserId() {
         long userIdMax = -1;
         String sql = "SELECT userId FROM users;";
@@ -151,7 +153,7 @@ public class JIBDBUtil {
         log.trace("userIdMax=" + userIdMax);
         return userIdMax;
     }
-
+    
     public long loadUsers() {
         long loadedUsers = 0L;
         String sql = "SELECT userId,_uuid,username,authtoken,admin,opt FROM users;";
@@ -166,20 +168,23 @@ public class JIBDBUtil {
         try {
             while (rs5 != null && rs5.next()) {
                 JIBUser opt = rs5.getObject("opt", JIBUser.class);
-                JIBUser tmpu = JavaIrcBouncer.jibCore.createUser(rs5.getString(3), rs5.getBoolean(5), true);
+                JIBUser tmpu = null;//JavaIrcBouncer.jibCore.createUser(rs5.getString(3), rs5.getBoolean(5), true);
+                tmpu = JavaIrcBouncer.jibCore.loadUser(opt, rs5.getString(3), rs5.getBoolean(5));
                 tmpu.setUserId(rs5.getLong(1));
                 tmpu.setUUID((UUID) rs5.getObject("_uuid"));
                 tmpu.setAuthToken(rs5.getString(4));
-
+                
                 log.debug("Contains User=" + rs5.getString(3));
                 loadedUsers++;
+                log.info("tmpu=" + tmpu.toString());
+                log.info("opt=" + opt.toString());
             }
         } catch (SQLException ex) {
             log.error((String) null, ex);
         }
         return loadedUsers;
     }
-
+    
     public void addUser(JIBUser u) {
         String sql = "INSERT INTO users (userId,_uuid,username,authtoken,admin,opt) VALUES(?,?,?,?,?,?);";
         PreparedStatement ps2 = null;
@@ -211,7 +216,7 @@ public class JIBDBUtil {
             log.error((String) null, ex);
         }
     }
-
+    
     public void removeUser(JIBUser u) {
         String sql = "DELETE FROM users WHERE _uuid = ?;";
         PreparedStatement ps2 = null;
@@ -224,7 +229,7 @@ public class JIBDBUtil {
             log.error((String) null, ex);
         }
     }
-
+    
     public void refreshUser(JIBUser u) {
         String sql = "UPDATE users SET opt = ? WHERE _uuid = ?;";
         PreparedStatement ps2 = null;
@@ -238,7 +243,7 @@ public class JIBDBUtil {
             log.error((String) null, ex);
         }
     }
-
+    
     public void addClientAuth(JIBUser u, String authToken) {
         String sql = "INSERT INTO clientauth (username,password) VALUES(?,?);";
         PreparedStatement ps2 = null;
@@ -251,7 +256,7 @@ public class JIBDBUtil {
             log.error((String) null, ex);
         }
     }
-
+    
     public void addServer(JIBUser u, String Server, int Port) {
         String sql = "INSERT INTO servers (server,port,u,ssl,ipv6) VALUES(?,?,?,?,?);";
         PreparedStatement ps2 = null;
@@ -265,51 +270,55 @@ public class JIBDBUtil {
             log.error((String) null, ex);
         }
     }
-
+    
     public void addServer(JIBUser u, JIBIRCServer serv) {
-        String sql = "INSERT INTO servers (server,port,u,ssl,ipv6,nick,username,realname,nsacct,nspass,channels,opt) VALUES(?,?,?,?,?,?,?,?,?,?,?,?);";
+        String sql = "INSERT INTO servers (s,server,port,u,ssl,ipv6,clientbind,serverpass,nick,username,realname,nsacct,nspass,channels,opt) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);";
         PreparedStatement ps2 = null;
         try {
             ps2 = getDatabase().prepareStatement(sql);
-            ps2.setString(1, serv.getServer());
-            ps2.setInt(2, serv.getPort());
-            ps2.setString(3, u.getUUID().toString());
-            ps2.setBoolean(4, serv.getSsl());
-            ps2.setBoolean(5, serv.getIpv6());
-            ps2.setString(6, serv.getNick());
-            ps2.setString(7, serv.getUser());
-            ps2.setString(8, serv.getRealname());
-            ps2.setString(9, serv.getNickServUser());
-            ps2.setString(10, serv.getNickServPass());
-            ps2.setString(11, serv.getChannels());
-            ps2.setObject(12, serv);
+            ps2.setString(1, serv.getUUID().toString());
+            ps2.setString(2, serv.getServer());
+            ps2.setInt(3, serv.getPort());
+            ps2.setString(4, u.getUUID().toString());
+            ps2.setBoolean(5, serv.getSsl());
+            ps2.setBoolean(6, serv.getIpv6());
+            ps2.setString(7, serv.getClientBind());
+            ps2.setString(8, serv.getPassword());
+            ps2.setString(9, serv.getNick());
+            ps2.setString(10, serv.getUser());
+            ps2.setString(11, serv.getRealname());
+            ps2.setString(12, serv.getNickServUser());
+            ps2.setString(13, serv.getNickServPass());
+            ps2.setString(14, serv.getChannels());
+            ps2.setObject(15, serv);
             ps2.execute();
         } catch (SQLException ex) {
             log.error((String) null, ex);
         }
     }
-
+    
     public void removeServer(JIBUser u, JIBIRCServer serv) {
-        String sql = "DELETE FROM servers WHERE server = ? AND port = ? AND u = ?;";
+        String sql = "DELETE FROM servers WHERE server = ? AND port = ? AND u = ? AND s = ?;";
         PreparedStatement ps2 = null;
         try {
             ps2 = getDatabase().prepareStatement(sql);
             ps2.setString(1, serv.getServer());
             ps2.setInt(2, serv.getPort());
             ps2.setString(3, u.getUUID().toString());
+            ps2.setString(4, serv.getUUID().toString());
             ps2.execute();
         } catch (SQLException ex) {
             log.error((String) null, ex);
         }
     }
-
+    
     public ArrayList<JIBIRCServer> getServers(JIBUser u) {
-        String sql = "SELECT u, server, port, ssl, ipv6, opt FROM servers WHERE u = ?;";
+        String sql = "SELECT u, s, server, port, ssl, ipv6,clientbind,serverpass,nick,username,realname,nsacct,nspass,channels, opt FROM servers WHERE u = ?;";
         ArrayList<JIBIRCServer> ret = null;
         ret = new ArrayList<JIBIRCServer>();
         PreparedStatement ps5 = null;
         ResultSet rs5 = null;
-
+        
         try {
             ps5 = getDatabase().prepareStatement(sql);
             ps5.setString(1, u.getUUID().toString());
@@ -317,7 +326,7 @@ public class JIBDBUtil {
         } catch (SQLException ex) {
             log.error((String) null, ex);
         }
-
+        
         try {
             //if (rs5 != null) {
             //    rs5.first();
@@ -325,21 +334,63 @@ public class JIBDBUtil {
             while (rs5 != null && rs5.next()) {
                 JIBIRCServer opt = rs5.getObject("opt", JIBIRCServer.class);
                 JIBIRCServer tmp1 = new JIBIRCServer();
-                tmp1.setServer(rs5.getString(2));
-                tmp1.setPort(rs5.getInt(3));
-                tmp1.setSsl(rs5.getBoolean(4));
-                tmp1.setIpv6(rs5.getBoolean(5));
-
+                tmp1.setUUID(UUID.fromString(rs5.getString(2)));
+                tmp1.setServer(rs5.getString(3));
+                tmp1.setPort(rs5.getInt(4));
+                tmp1.setSsl(rs5.getBoolean(5));
+                tmp1.setIpv6(rs5.getBoolean(6));
+                tmp1.setClientBind(rs5.getString(7));
+                tmp1.setPassword(rs5.getString(8));
+                tmp1.setNick(rs5.getString(9));
+                tmp1.setUser(rs5.getString(10));
+                tmp1.setRealname(rs5.getString(11));
+                tmp1.setNickServUser(rs5.getString(12));
+                tmp1.setNickServPass(rs5.getString(13));
+                tmp1.addChannels(rs5.getString(14));
+                
                 log.debug("Contains Server=" + tmp1 + " tmp1.getServer()=" + tmp1.getServer() + " tmp1.getPort()=" + tmp1.getPort());
                 ret.add(tmp1);
             }
         } catch (SQLException ex) {
             log.error((String) null, ex);
         }
-
+        
         return ret;
     }
-
+    
+    public void loadServers(JIBUser u) {
+        JIBUser u2 = null;
+        ArrayList<JIBIRCServer> sl2 = null;
+        List<JIBIRCServer> sl2s = null;
+        if (u == null) {
+            Iterator<JIBUser> it2 = JavaIrcBouncer.jibCore.getUsers();
+            synchronized (it2) {
+                while (it2.hasNext()) {
+                    u2 = it2.next();
+                    sl2 = getServers(u2);
+                    sl2s = java.util.Collections.synchronizedList(sl2);
+                    synchronized (sl2s) {
+                        Iterator<JIBIRCServer> sl2sit1 = sl2s.iterator();
+                        while (sl2sit1.hasNext()) {
+                            u2.addIrcServer(sl2sit1.next());
+                        }
+                    }
+                }
+            }
+        } else {
+            ArrayList<JIBIRCServer> sl1 = null;
+            List<JIBIRCServer> sl1s = null;
+            sl1 = getServers(u);
+            sl1s = java.util.Collections.synchronizedList(sl1);
+            synchronized (sl1s) {
+                Iterator<JIBIRCServer> sl1sit1 = sl1s.iterator();
+                while (sl1sit1.hasNext()) {
+                    u2.addIrcServer(sl1sit1.next());
+                }
+            }
+        }
+    }
+    
     public void addChannel(JIBUser u, String Channel) {
         if (containsChannel(u, Channel) != 0) {
             return;
@@ -356,7 +407,7 @@ public class JIBDBUtil {
             log.error((String) null, ex);
         }
     }
-
+    
     public void removeChannel(JIBUser u, String Channel) {
         if (containsChannel(u, Channel) < 1) {
             return;
@@ -372,7 +423,7 @@ public class JIBDBUtil {
             log.error((String) null, ex);
         }
     }
-
+    
     public String[] getChannels(JIBUser u) {
         Vector<String> cv = new Vector<String>();
         String[] channels = new String[100];
@@ -400,7 +451,7 @@ public class JIBDBUtil {
         channels = (String[]) cv.toArray(channels);
         return channels;
     }
-
+    
     public long containsChannel(JIBUser u, String chan) {
         long count = -1;
         PreparedStatement ps5 = null;
@@ -419,7 +470,7 @@ public class JIBDBUtil {
         }
         return count;
     }
-
+    
     public void logUserTargetMessage(JIBUser u, JIBIRCServer s, String logUser, String logTarget, String logMessage) {
         String sql = "INSERT INTO log1 (loguser,logtarget,logmessage,u,s) VALUES(?,?,?,?,?);";
         PreparedStatement ps2 = null;
@@ -436,7 +487,7 @@ public class JIBDBUtil {
             log.error((String) null, ex);
         }
     }
-
+    
     public ArrayList<String> replayLog(JIBUser u) {
         String sql = "SELECT loguser,logtarget,logmessage FROM log1 WHERE u = ?;";
         ArrayList<String> replay = new ArrayList<String>();
@@ -474,7 +525,7 @@ public class JIBDBUtil {
         }
         return replay;
     }
-
+    
     public void clearLog(JIBUser u) {
         if (u != null) {
             String sql = "DELETE FROM log1 WHERE u = ?;";
@@ -499,7 +550,7 @@ public class JIBDBUtil {
             }
         }
     }
-
+    
     public void clearLogOlderThan2Days() {
         String sql = "DELETE FROM log1 WHERE NOW() - ts1 > INTERVAL '2' DAY;";
         PreparedStatement ps2 = null;
@@ -511,7 +562,7 @@ public class JIBDBUtil {
             log.error((String) null, ex);
         }
     }
-
+    
     public boolean checkUserPass(String User, String Pass) {
         String sql = "SELECT COUNT(*) FROM clientauth WHERE username = ? AND password = ?;";
         PreparedStatement ps9 = null;
