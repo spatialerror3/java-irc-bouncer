991280cef192528a0ab1baace8363f5740e4e17c
spatialerror3 <spatialerror3@protonmail.com>
2025-09-03 19:35:46 +0200

    fix

--- src/main/java/net/spatialerror3/jib/JIBIRCLineProcessing.java
+++ src/main/java/net/spatialerror3/jib/JIBIRCLineProcessing.java
@@ -23,5 +23,5 @@ package net.spatialerror3.jib;
  */
 public interface JIBIRCLineProcessing {
 
-    public void processLine(JIBUser u, JIBIRC irc, JIBIRCServer s, String l);
+    public void processLine(JIBUser _u, JIBIRC _i, JIBIRCServer _s, String l);
 }
