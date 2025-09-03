7b8eda61e5c37770d5db9ac918c03ce07547d60a
spatialerror3 <spatialerror3@protonmail.com>
2025-09-03 19:01:52 +0200

    fix

--- src/main/java/net/spatialerror3/jib/JIBUserInfo.java
+++ src/main/java/net/spatialerror3/jib/JIBUserInfo.java
@@ -24,58 +24,63 @@ import java.io.Serializable;
  * @author spatialerror3
  */
 public class JIBUserInfo implements Serializable {
-
+    
     private static final long serialVersionUID = 1L;
     public String nick;
     public String user;
     public String host;
     public String realname;
-
+    
     public String getNick() {
         return this.nick;
     }
-
+    
     public String getUser() {
         return this.user;
     }
-
+    
     public String getHost() {
         return this.host;
     }
-
+    
     public String getRealname() {
         return this.realname;
     }
-
+    
     public String nuh() {
         return nick + "!" + user + "@" + host;
     }
-
+    
     public void setNick(String nick) {
         this.nick = nick;
     }
-
+    
     public void setUser(String user) {
         this.user = user;
     }
-
+    
     public void setHost(String host) {
         this.host = host;
     }
-
+    
     public void setRealname(String realname) {
         this.realname = realname;
     }
-
+    
     public static JIBUserInfo parseNUH(String containsNUH) {
         JIBUserInfo ret = null;
         String tmp1 = JIBStringUtil.remDD(containsNUH);
-        String[] tmp2 = tmp1.split("!", 2);
-        String[] tmp3 = (tmp2[1]).split("@", 2);
-        ret = new JIBUserInfo();
-        ret.setNick(tmp2[0]);
-        ret.setUser(tmp3[0]);
-        ret.setHost(tmp3[1]);
+        if (tmp1.indexOf('!') != -1 && tmp1.indexOf('@') != -1) {
+            String[] tmp2 = tmp1.split("!", 2);
+            String[] tmp3 = (tmp2[1]).split("@", 2);
+            ret = new JIBUserInfo();
+            ret.setNick(tmp2[0]);
+            ret.setUser(tmp3[0]);
+            ret.setHost(tmp3[1]);
+        } else {
+            ret = new JIBUserInfo();
+            ret.setHost(tmp1);
+        }
         return ret;
     }
 }
