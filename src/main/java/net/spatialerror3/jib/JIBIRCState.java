1b71e423c9c4ee2d6aa51232664b2969d6db3dd4
spatialerror3 <spatialerror3@protonmail.com>
2025-09-16 18:42:32 +0200

    fix

---
+++ src/main/java/net/spatialerror3/jib/JIBIRCState.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2025 spatialerror3
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package net.spatialerror3.jib;
+
+/**
+ *
+ * @author spatialerror3
+ */
+public class JIBIRCState implements JIBIRCLineProcessing {
+    private JIBUser _u = null;
+    private JIBIRC _i = null;
+    private JIBIRCServer _s = null;
+    
+    public JIBIRCState(JIBUser _u, JIBIRC _i, JIBIRCServer _s) {
+        this._u = _u;
+        this._i = _i;
+        this._s = _s;
+    }
+    
+    @Override
+    public void processLine(JIBUser _u, JIBIRC _i, JIBIRCServer _s, String l) {
+        
+    }
+    
+}
